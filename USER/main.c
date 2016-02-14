05a4124d8f2ad465c9ebbcb1285b730d046e1dfb
anonymous-void <anonymous4reg@gmail.com>
2016-02-14 17:41:40 +0800

    After delete the project accidently, i rewrote it

--- USER/main.c
+++ USER/main.c
@@ -14,16 +14,21 @@
 
 #define RELAY_ON        0
 #define RELAY_OFF       1
+#define WINDOW_WIDTH    10
+#define ZERO_SET_WINDOW_WIDTH   100
 
 // Pressure measurement
-u32 gu32_sensorRead = 0;
-u32 gu32_window_a[10] = {0}, gu32_avrg = 0;
-u32 gu32_setZeroDat = 0; // Used for storing the value for setting zero
-u32 gu32_scale = 1;
-u32 gu32_realWeight = 0;
-double gd_window_a[10] = {0}, gd_avrg = 0;
-double gd_setZeroDat = 0; // Used for storing the value for setting zero
+int32_t gi32_sensorRead = 0;
+int32_t gi32_window_a[WINDOW_WIDTH] = {0};
+int32_t gi32_zeroSetWindow_a[ZERO_SET_WINDOW_WIDTH] = {0};
+
+int32_t gi32_avrg = 0;
+int32_t gi32_zeroSetAvrg = 0;
+
+int32_t gi32_setZeroDat = 0; // Used for storing the value for setting zero
+double gd_scale = 213.34278867748;
 double gd_realWeight = 0;
+
 
 // USART data transfer
 u8 gu8_dataTxState = 0; // 数据传输状态，0为停止，1为开始传输
@@ -33,10 +38,17 @@ u8 gu8_relayARMState = 0;
 u8 gu8_relayFireState = 0;
 
 unsigned long ReadCount(void);
+int32_t getValue(void);
+void fillWindow( int32_t window[], int length, int32_t val );
+int32_t getWindowAvg( int32_t window[], int length );
+
+void zeroSetFillWindow( int32_t window[], int length, int32_t val );
+int32_t zeroSetGetWindowAvg( int32_t window[], int length );
+
 
 int main()
 {
-    u8 u8_len, u8_t, u8_wCnt = 0;
+    u8 u8_len, u8_t;
     char c_cmdCode_a[10] = {0};
     //------------Initialization------------
     RCC_Configuration();
@@ -50,23 +62,20 @@ int main()
 
     while(1)
     {
-        gu32_sensorRead = ReadCount();
-        gu32_window_a[u8_wCnt] = gu32_sensorRead;
-        u8_wCnt ++;
-        if (u8_wCnt > 10)
-        {
-            u8_wCnt = 0;
-        }
+        gi32_sensorRead = getValue();
+        
+        fillWindow( &gi32_window_a[0], WINDOW_WIDTH, gi32_sensorRead );
+        gi32_avrg = getWindowAvg( &gi32_window_a[0], WINDOW_WIDTH );
+        
+        zeroSetFillWindow( &gi32_zeroSetWindow_a[0], ZERO_SET_WINDOW_WIDTH, gi32_sensorRead);
+        gi32_zeroSetAvrg = zeroSetGetWindowAvg( &gi32_zeroSetWindow_a[0], ZERO_SET_WINDOW_WIDTH );
+        
+        gd_realWeight = ( (double)(gi32_avrg) - (double)gi32_setZeroDat ) / gd_scale;
 
-        gu32_avrg = (gu32_window_a[0] + gu32_window_a[1] + gu32_window_a[2] + \
-                     gu32_window_a[3] + gu32_window_a[4] + gu32_window_a[5] + \
-                     gu32_window_a[6] + gu32_window_a[7] + gu32_window_a[8] + \
-                     gu32_window_a[9]) / 10;
-
-        gu32_realWeight = (gu32_avrg - gu32_setZeroDat) / gu32_scale * 20;
+        
+        printf("%f, %i\r\n", gd_realWeight, gi32_zeroSetAvrg);
         if ( 1 == gu8_dataTxState )
         {
-            printf("w = %u, av = %u, zs = %u\r\n", gu32_sensorRead, gu32_avrg, gu32_realWeight);
             pin_ledGreen = !pin_ledGreen;
         }
         // Income Command Analyze        
@@ -80,8 +89,6 @@ int main()
             for(u8_t = 0; u8_t < u8_len; u8_t++)
             {
                 c_cmdCode_a[u8_t] = USART_RX_BUF[u8_t]; // 将Buffer中的每个char放到cmdCode中
-//                USART1->DR=USART_RX_BUF[u8_t];
-//                while((USART1->SR&0X40)==0);
             }
             printf("\n\n");
             USART_RX_STA=0;
@@ -91,7 +98,7 @@ int main()
             else if ( (0 == strcmp(c_cmdCode_a, "STOP")) || (0 == strcmp(c_cmdCode_a, "stop")) )
                 gu8_dataTxState = 0;
             else if ( (0 == strcmp(c_cmdCode_a, "ZERO")) || (0 == strcmp(c_cmdCode_a, "zero")) )
-                gd_setZeroDat = gd_avrg;
+                gi32_setZeroDat = gi32_zeroSetAvrg;
             else if ( (0 == strcmp(c_cmdCode_a, "ARM")) || (0 == strcmp(c_cmdCode_a, "arm")) )
                 gu8_relayARMState = 1;
             else if ( (0 == strcmp(c_cmdCode_a, "FIRE")) || (0 == strcmp(c_cmdCode_a, "fire")) )
@@ -105,7 +112,7 @@ int main()
             if ( 1 == gu8_relayFireState )
             {
                 pin_FIRE = RELAY_ON; // Relay ON
-                delay_ms(500); // After 500 miliseconds, clear both ARM & FIRE flags.
+                delay_ms(1500); // After 500 miliseconds, clear both ARM & FIRE flags.
                 pin_ARM = RELAY_OFF; //Relay OFF
                 pin_FIRE = RELAY_OFF;
                 gu8_relayARMState = 0;
@@ -113,7 +120,7 @@ int main()
             }
         }
 
-        delay_ms(200);
+        delay_ms(150);
     }
 }
 
@@ -133,7 +140,82 @@ unsigned long ReadCount(void)
             Count++;
     }
     pin_SCK = 1;
-    Count ^= 0x800000;
+    //Count ^= 0x800000;
     pin_SCK = 0;
     return (Count);
+}
+
+int32_t getValue(void)
+{
+    u32 val_raw = 0;
+    int32_t val4ret = 0;
+    
+    val_raw = ReadCount();
+    if ( val_raw & 0x800000 )
+    {
+        val_raw = (~val_raw) + 0x01; // Two's complement it to get its abs value
+        val4ret = 0;
+        val4ret |= val_raw;
+        val4ret = (~val4ret) + 0x01; // Convert 24bit Two's complement value to 32bit
+    }
+    else
+    {
+        val4ret = 0;
+        val4ret |= val_raw;
+    }
+    
+    return val4ret;
+}
+
+void fillWindow( int32_t window[], int length, int32_t val )
+{
+    static int count_fw = 0;
+    window[count_fw] = val;
+    if ( count_fw > length )
+    {
+        count_fw = 0;
+    }
+    else
+    {
+        count_fw ++;
+    }
+}
+
+
+int32_t getWindowAvg( int32_t window[], int length )
+{
+    int count = 0;
+    int32_t sum = 0;
+    for (count = 0; count < length; count ++ )
+    {
+        sum += window[count];
+    }
+    
+    return (sum / length);
+}
+
+void zeroSetFillWindow( int32_t window[], int length, int32_t val )
+{
+    static int count_fw = 0;
+    window[count_fw] = val;
+    if ( count_fw > length )
+    {
+        count_fw = 0;
+    }
+    else
+    {
+        count_fw ++;
+    }
+}
+
+int32_t zeroSetGetWindowAvg( int32_t window[], int length )
+{
+    int count = 0;
+    int32_t sum = 0;
+    for (count = 0; count < length; count ++ )
+    {
+        sum += window[count];
+    }
+    
+    return (sum / length);
 }
